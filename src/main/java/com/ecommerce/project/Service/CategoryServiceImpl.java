25d27bfa171b15e9379b383be42df23676a3abdb
corbin <corbin@gmail.com>
2025-02-02 11:21:33 +0800

    DeleteCategory -> new ResponseStatusException

--- src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.Service;
 
 import com.ecommerce.project.Model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +25,9 @@ public class CategoryServiceImpl implements CateGoryService{
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream()
                 .filter(category1 -> category1.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-        if (category == null) return "Category not found.";
+                .findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
+
         categories.remove(category);
         return categoryId + " is delete successful";
     }
